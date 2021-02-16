b52e0af9c50c954d41ed62c8f47411ed5c49bb15
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-16 04:11:13 -0500

    Connecting to the mongoDB atlas database.

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// using cloud atlas version of mongodb
+const CONNECTION_URL="mongodb+srv://santoryuu:fVP230vQBAQo@cluster0.8dkil.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
