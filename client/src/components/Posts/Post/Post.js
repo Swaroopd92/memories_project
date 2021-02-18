45f5994744b36ed5d64470e4578befe0e7d4ec54
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-18 00:46:56 -0500

    Added styles for Post, and Posts.

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,44 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
 import React from 'react';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+
 import useStyles from './styles';
 
-
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
-    return(
-        <h1>POST</h1>
+    return (
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: 'white' }} size="small" onClick={() => { }}>
+                    <MoreHorizIcon font="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button style={{color: 'rgba(170,51,51, 1)'}} size="small" onClick={() => { }}>
+                    <ThumbUpAltIcon font="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button style={{color: 'rgba(170,51,51, 1)'}} size="small" onClick={() => { }}>
+                    <DeleteIcon font="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     );
 }
 
